feabfe94c62673ffd13a3fb494b2c6f9d6d46399
Alsu <alsousha@mail.ru>
2020-12-20 05:50:15 +0200

    added parameters in methods PostManager.search, PostManager.delete

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -4,10 +4,10 @@ import ru.netology.domain.Post;
 
 public class PostManager {
     private Post[] posts;
-    public Post[] search(){
+    public Post[] search(int ownerId, int domain, String query, boolean ownersOnly, int count, int offset){
         return null;
     }
-    public void delete(){
+    public void delete(int ownerId, int postId){
 
     }
 }
